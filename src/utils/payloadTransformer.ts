3c6e178cf57c84040f2741de3c8cdf48e8c9609c
Fran <187207872+Reyunix@users.noreply.github.com>
2025-10-04 00:54:00 +0200

    refactoring GenerateForm component

--- src/utils/payloadTransformer.ts
+++
@@ -1,6 +0,0 @@
-import type { LoginForm } from "../schemas/loginFormSchema"
-
-export const payloadTransformer = (data: LoginForm) => {
-
-  return {userEmail: data.userName, userPassword: data.userPassword} 
-}
