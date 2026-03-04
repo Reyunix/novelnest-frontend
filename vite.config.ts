ef175afae99662dfa00964e7f1363f1abc23eee2
Fran <187207872+Reyunix@users.noreply.github.com>
2026-03-04 03:22:17 +0100

    chore: deleted the build visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react()],
   server: {
     https: false,
     port: 5173,
